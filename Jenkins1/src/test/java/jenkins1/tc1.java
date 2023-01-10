5292f25f6e21a6ff618d9a7076b01db71afb0411
miriyalabittu <miriyalabittu@localhost>
2023-01-10 16:09:59 +0530

    jenkins1

---
+++ Jenkins1/src/test/java/jenkins1/tc1.java
@@ -0,0 +1,13 @@
+package jenkins1;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc1 {
+	@Test
+	public void m1() {
+		Reporter.log("love u",true);
+	}
+
+}
---
+++ Jenkins1/src/test/java/jenkins1/tc2.java
@@ -0,0 +1,14 @@
+package jenkins1;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc2 {
+	@Test
+	public void m2() {
+		Reporter.log("hate u",true);
+	}
+
+
+}
